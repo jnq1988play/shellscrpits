6894db5aaacfb3401e6db9f35d68c0d81fb34779
jnq1988 <1369934140@qq.com>
2021-03-15 10:55:27 +0800

    优化更改ssh地址为变量

--- autobackup.sh
+++ autobackup.sh
@@ -5,7 +5,7 @@ BACKUPPATH=/shell/
 cd $BACKUPPATH
 tar cvPf "$tarname".tar.gz $BACKUPPATH*
 rsync -avz $BACKUPPATH*.tar.gz $backhost:/backup
-ssh root@10.0.0.31 "ls -al | grep *.tar.gz;if [ $? -eq 0 ] ;then echo backup complate ;fi"
+ssh root@$backhost "ls -al | grep *.tar.gz;if [ $? -eq 0 ] ;then echo backup complate ;fi"
 if [ $? -eq 0 ]
 then
 	rm -rf /$BACKUPPATH/*.tar.gz
