47dddd9c061e4db91308e2e9452616865c1383b3
jnq1988 <1369934140@qq.com>
2021-03-15 16:06:05 +0800

    批量创建用户

--- useradds3.sh
+++ useradds3.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-read -p "创建用户数量:" num
-for i in `seq $num`
+read -p "创建用户数量:" num ####输入创建用户的数量
+for i in `seq $num` ####根据数量循环次数
 do
-UserName=User$i
-Passwd=`echo $RANDOM |md5sum |cut -c 8-15`
+UserName=User$i ####循环内设置用户名变量
+Passwd=`echo $RANDOM |md5sum |cut -c 8-15` ####随机生成密码
 useradd -m -s /bin/bash $UserName ####创建用户
 echo $Passwd | passwd --stdin $UserName ####指定密码
 echo "用户已创建完成" ####打印信息
