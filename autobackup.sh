1743a4a5f3dec21203b98f8f0198ee01db5c8fcf
jnq1988 <1369934140@qq.com>
2021-03-15 11:00:58 +0800

    添加注释

--- autobackup.sh
+++ autobackup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-backhost=10.0.0.31
-tarname=shells_`date +"%Y-%m-%d"`
-BACKUPPATH=/shell/
-cd $BACKUPPATH
-tar cvPf "$tarname".tar.gz $BACKUPPATH*
-rsync -avz $BACKUPPATH*.tar.gz $backhost:/backup
-ssh root@$backhost "ls -al | grep *.tar.gz;if [ $? -eq 0 ] ;then echo backup complate ;fi"
-if [ $? -eq 0 ]
+backhost=10.0.0.31  ###设置保存文件的目标主机ip
+tarname=shells_`date +"%Y-%m-%d"` ###设置tar文件名 根据时间生产
+BACKUPPATH=/shell/ ###数据源地址
+cd $BACKUPPATH ###跳转至源地址
+tar cvPf "$tarname".tar.gz $BACKUPPATH* ###打包数据并以生成时间命名
+rsync -avz $BACKUPPATH*.tar.gz $backhost:/backup  ###rsync增量备份至目标主机位置
+ssh root@$backhost "ls -al | grep *.tar.gz;if [ $? -eq 0 ] ;then echo backup complate ;fi" ###判断目标主机是否已经接受压缩文件
+if [ $? -eq 0 ] ###如果压缩完成则删除本地生成的压缩文件的
 then
 	rm -rf /$BACKUPPATH/*.tar.gz
 fi
