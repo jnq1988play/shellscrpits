5ae39f398281fa6a435b71e4e8e1763649092a81
jnq1988 <1369934140@qq.com>
2021-03-19 14:49:35 +0800

    检测cpu负载

--- cpuused.sh
+++ cpuused.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-Used=$(uptime |awk '{print $(NF-2})' | sed 's/,//g')
-#um=$(grep 'model name' /proc/cpuinfo | wc -l)
+Used=$(uptime |awk '{print $(NF-2)}' | sed 's/,//g')
+UNum=$(echo $Used*100 | bc | sed "s/\..*//g")
+Cpunum=$(grep 'model name' /proc/cpuinfo | wc -l)
+CNum=$(echo $Cpunum*100-50 | bc | sed "s/\..*//g")
 i=1
 while [ $i -le 5 ]
 do
-if [ $Used -ge 2 ]
+if [ $UNum -ge $CNum ]
 then
-    echo "CPU负载过高"|mail -s "CPU负载过高"  XXXXXXXXXX@XXX.com
+    echo "CPU负载过高" #|#mail -s "CPU负载过高"  XXXXXXXXXX@XXX.com
+else
+    echo "it's ok"
 fi
 let i++
 sleep 5s
